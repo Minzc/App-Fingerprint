d72e6f675ed3debf725e09a6862bebdbdaea5f2d
Minzc <congzicun@gmail.com>
2015-04-27 17:11:26 -0700

    add debug option

--- algo.py
+++ algo.py
@@ -7,6 +7,8 @@ from collections import defaultdict
 import operator
 
 
+DEBUG = True
+
 class TreeNode:
   def __init__(self, father, value):
     self._children = {}
@@ -119,30 +121,27 @@ class KVTree:
           treePath = (hst, key, package.app, value) 
           self._add_node(treePath, addInfo) 
 
-  def _gen_rules(self, serviceKey, confidence, support):
-    root = self._root
-    hostnodes = root.children.values()
+  def _gen_rules(self, goodCandidates, confidence, support):
+    hostnodes = self._root.children.values()
     rules = []
     for hostNode in hostnodes:
         hostName = hostNode.value
         for tokenNode in hostNode.children.values():
             tokenName = tokenNode.value
             validToken = False
-            # number of app's use this token
+            # number of app's using this token
             tokenSupport = len(tokenNode.children)
             tokenConfidence = 1.0 * tokenNode.count / hostNode.count
-            print 'DEBUG', hostNode.value.encode(
+            if DEBUG : print '>>> [DEBUG:gen_rules]', hostNode.value.encode(
                 'utf-8'), hostNode.count, 'tokensupport:', tokenSupport, 'tokenConfidence:', tokenConfidence, tokenName.encode(
                 'utf-8')
 
             if tokenNode.status == 1:
-                # check token confidence and token support
-                # if tokenConfidence < confidence or tokenSupport < support:
-                if tokenSupport < support:
-                    continue
-                validToken = True
+                # check if the pattern can satify the support and confidence requirement
+                if tokenConfidence >= confidence and tokenSupport >= support:
+                  validToken = True
 
-            if hostName in serviceKey.get() and tokenName in serviceKey.get()[
+            if hostName in goodCandidates and tokenName in goodCandidates[
                 hostName] and tokenNode.status != -1:
                 validToken = True
 
@@ -189,7 +188,6 @@ class ParamRules:
     def load_rules(ln): 
       host, rule = ln.split(':')
       rule = [host] + filter(None, rule.split('$'))
-      print rule
       self._rules.append(rule)
 
     loadfile('ad_dict.txt', load_rules)
@@ -254,98 +252,69 @@ class ParamRules:
         rst.add(v)
     return rst
 
-def KVMiner(training_data=None, confidence=0.8, support=2): 
+
+def test_rule_miner_train():
+  def tuples2str(tuples):
+    return '&'.join([k+'='+v for k, v in tuples ])
+  paraminer = ParamRules()
+  patterns = []
+  print ">>> Starting processing packages"
+  counter = 0
+  for record in load_pkgs():
+    subpatterns = list(paraminer.mine(record))
+    patterns += subpatterns
+    if len(subpatterns) > 0:
+      counter += 1
   sqldao = SqlDao()
   sqldao.execute('DELETE FROM patterns WHERE kvpattern IS NOT NULL')
+  QUERY = 'INSERT INTO patterns (label, support, confidence, host, kvpattern) VALUES (%s, %s, %s, %s, %s)'
+  for recog_rule in paraminer.stat(patterns):
+    k, app, host, confidence, support = recog_rule
+    k = tuples2str(k)
+    sqldao.execute(QUERY, (app, support, confidence, host, k))
+    if DEBUG : print ">>> [DEBUG]", k, app, host, confidence, support
+  print counter
 
-  training_data = load_pkgs() if not training_data else training_data
+
+
+def KVMiner(training_data=None, confidence=0.8, support=2): 
+  def tuples2str(tuples):
+    return '&'.join([k+'='+v for k, v in tuples ])
+
+  sqldao = SqlDao()
+  sqldao.execute('DELETE FROM patterns WHERE kvpattern IS NOT NULL')
+  training_data = load_pkgs(1000) if not training_data else training_data
+
+  paraminer = ParamRules()
+  patterns = []
 
   tree = KVTree()
-  serviceKey = Relation()
+  goodCandidates = defaultdict(lambda : defaultdict(int))
 
   # Build the forest
   for package in training_data:
+    subpatterns = list(paraminer.mine(package))
+    patterns += subpatterns
+
     tree._build_tree(package, package.querys.items())
     for k, v in package.querys.items():
       if package.app in v or package.name in v:
-        serviceKey.add(package.secdomain, k)
-  print 'DEBUG', serviceKey
+        goodCandidates[package.secdomain][k] += 1
+  if DEBUG : print '>>> [DEBUG:kvminer]', goodCandidates
   
   tree._prune_forest()
 
-  rules = tree._gen_rules(serviceKey, confidence, support)
 
   # Persist
-  sqldao = SqlDao()
+  QUERY = 'INSERT INTO patterns (label, support, confidence, host, kvpattern) VALUES (%s, %s, %s, %s, %s)'
+  for recog_rule in paraminer.stat(patterns):
+    k, app, host, confidence, support = recog_rule
+    k = tuples2str(k)
+    sqldao.execute(QUERY, (app, support, confidence, host, k))
   # app, value, token, host
-  for appName, appCompany, valueName, tokenName, hostName, tokenConfidence, tokenSupport in rules:
-      sqldao.execute(
-          'INSERT INTO patterns (label, support, confidence, host, kvpattern) VALUES (%s, %s, %s, %s, %s)',
-          (appName, tokenSupport, tokenConfidence, hostName, tokenName+'='+valueName))
-
+  for appName, appCompany, valueName, tokenName, hostName, tokenConfidence, tokenSupport in tree._gen_rules(goodCandidates, confidence, support):
+    sqldao.execute(QUERY , (appName, tokenSupport, tokenConfidence, hostName, tokenName+'='+valueName))
   sqldao.close()
-
-
-def _buildTestTree():
-    sqldao = SqlDao()
-    query = 'SELECT app, company, token, host, value FROM features'
-    tree = KVTree()
-    vnodes = {}
-
-    # Build the tree
-    for app, company, token, host, value in sqldao.execute(query):
-        treePath = (host, token, value, app)
-        tree._add_node( treePath )
-
-    sqldao.close()
-    return tree._root
-
-
-def _test(package, root):
-    hst = package.secdomain
-    app = package.app
-    company = package.company
-    predict_app = None
-
-    if hst:
-        predict = None
-        for key, values in package.querys.items():
-            for value in filter(None, values):
-                treePath = (hst, key, value)
-                print ">>> [DEBUG] treePath is", treePath
-                treeNode = root
-                while len(treePath) > 0 and treeNode != None:
-                    treeNode = treeNode.search(treePath[0])
-                    treePath = treePath[1:]
-                # treeNode is valuenode
-                if treeNode:
-                    appNodes = treeNode.children.values()
-                    print appNodes, treeNode.value
-                    if len(appNodes) == 1:
-                        predict = (appNodes[0], None)
-                        predict_app = (appNodes[0].value, None)
-
-        if predict:
-            # predict company
-            if not predict[1]:
-                if predict[1] != package.app:
-                    predict = predict[0]
-                    print predict.value, app
-                    value = predict.father[0].value
-                    token = predict.father[0].father[0].value
-                    host = predict.father[0].father[0].father[0].value
-                    print "rules: %s\t%s\t%s" % (value, token, host)
-            # predict app
-            else:
-                predict = predict[0]
-                if predict.get_value() != package.company:
-                    print predict.get_value(), app
-                    value = predict.get_father()[0].get_value()
-                    token = predict.get_father()[0].get_father()[0].get_value()
-                    host = predict.get_father()[0].get_father()[0].get_father()[0].get_value()
-                    print "rules: %s\t%s\t%s" % (value, token, host)
-    # DEBUG
-    return predict_app
 
 
 def test_algo(test_set=None):
@@ -388,29 +357,6 @@ def test_algo(test_set=None):
     print 'Total:%s\tCorrect:%s\tWrong:%s' % (len(test_set), correct, wrong)
     return rst
 
-
-def test_rule_miner_train():
-  def tuples2str(tuples):
-    return '&'.join([k+'='+v for k, v in tuples ])
-  paraminer = ParamRules()
-  patterns = []
-  print ">>> Starting processing packages"
-  counter = 0
-  for record in load_pkgs():
-    subpatterns = list(paraminer.mine(record))
-    patterns += subpatterns
-    if len(subpatterns) > 0:
-      counter += 1
-  sqldao = SqlDao()
-  sqldao.execute('DELETE FROM patterns WHERE kvpattern IS NOT NULL')
-  QUERY = 'INSERT INTO patterns (label, support, confidence, host, kvpattern) VALUES (%s, %s, %s, %s, %s)'
-  for recog_rule in paraminer.stat(patterns):
-    k, app, host, confidence, support = recog_rule
-    k = tuples2str(k)
-    sqldao.execute(QUERY, (app, support, confidence, host, k))
-    print k, app, host, confidence, support
-  print counter
-
 def test_rule_miner_test():
   paraminer = ParamRules()
   paraminer.load_rules()
@@ -434,3 +380,65 @@ if __name__ == '__main__':
       test_rule_miner_train()
     elif sys.argv[1] == 'test_rules':
       test_rule_miner_test()
+
+
+
+# def _test(package, root):
+#     hst = package.secdomain
+#     app = package.app
+#     company = package.company
+#     predict_app = None
+
+#     if hst:
+#         predict = None
+#         for key, values in package.querys.items():
+#             for value in filter(None, values):
+#                 treePath = (hst, key, value)
+#                 print ">>> [DEBUG] treePath is", treePath
+#                 treeNode = root
+#                 while len(treePath) > 0 and treeNode != None:
+#                     treeNode = treeNode.search(treePath[0])
+#                     treePath = treePath[1:]
+#                 # treeNode is valuenode
+#                 if treeNode:
+#                     appNodes = treeNode.children.values()
+#                     print appNodes, treeNode.value
+#                     if len(appNodes) == 1:
+#                         predict = (appNodes[0], None)
+#                         predict_app = (appNodes[0].value, None)
+
+#         if predict:
+#             # predict company
+#             if not predict[1]:
+#                 if predict[1] != package.app:
+#                     predict = predict[0]
+#                     print predict.value, app
+#                     value = predict.father[0].value
+#                     token = predict.father[0].father[0].value
+#                     host = predict.father[0].father[0].father[0].value
+#                     print "rules: %s\t%s\t%s" % (value, token, host)
+#             # predict app
+#             else:
+#                 predict = predict[0]
+#                 if predict.get_value() != package.company:
+#                     print predict.get_value(), app
+#                     value = predict.get_father()[0].get_value()
+#                     token = predict.get_father()[0].get_father()[0].get_value()
+#                     host = predict.get_father()[0].get_father()[0].get_father()[0].get_value()
+#                     print "rules: %s\t%s\t%s" % (value, token, host)
+#     # DEBUG
+#     return predict_app
+
+# def _buildTestTree():
+#     sqldao = SqlDao()
+#     query = 'SELECT app, company, token, host, value FROM features'
+#     tree = KVTree()
+#     vnodes = {}
+
+#     # Build the tree
+#     for app, company, token, host, value in sqldao.execute(query):
+#         treePath = (host, token, value, app)
+#         tree._add_node( treePath )
+
+#     sqldao.close()
+#     return tree._root
