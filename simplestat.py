e9489db1066079f227941017457f70488028bff1
Minzc <congzicun@gmail.com>
2015-06-21 15:32:02 -0700

    convert-meta

--- simplestat.py
+++ simplestat.py
@@ -791,7 +791,7 @@ def batchTest(outputfile):
             continue
   fw = open(outputfile+'.rst', 'w')
   for rule in ruleCover:
-    fw.write("%s\t%s\t%s\t%s\n" % ( rule.secdomain, rule.key, rule.score, rule.app))
+    fw.write("%s\t%s\t%s\t%s\t%s\n" % ( rule.secdomain, rule.key, rule.score, rule.app, ruleCover[rule]))
   fw.write("Covered App:" + str(len(covered_app)) + "\n")
   fw.close()
   
