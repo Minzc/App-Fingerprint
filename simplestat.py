f2042b5fa7cfddd4181a375293d1b78dcda8a08e
Minzc <congzicun@gmail.com>
2015-06-23 10:32:44 -0700

    convert-meta

--- simplestat.py
+++ simplestat.py
@@ -866,7 +866,7 @@ def batchTest(outputfile):
         if value[0] == value[1]:
           precision += 1
           covered_app.add(value[1])
-    print "Precision: %s Recall: %s App: %s" % (float(precision)/total, float(recall).total, len(covered_app))
+    print "Precision: %s Recall: %s App: %s" % (float(precision)/total, float(recall) / total, len(covered_app))
 
 
 
