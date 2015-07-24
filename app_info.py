919c57dedebb2f87e26ee36b7123db52b7359547
Minzc <congzicun@gmail.com>
2015-07-24 16:22:58 -0700

    add category to appinfo

--- app_info.py
+++ app_info.py
@@ -1,25 +1,26 @@
 import consts
 from sqldao import SqlDao
 class App:
-  def __init__(self, package, name, company, trackId, website, app_type):
+  def __init__(self, package, name, company, trackId, website, category,app_type):
     self.package = package
     self.name = name if name else 'UNK'
     self.company = company if company else 'UNK'
     self.trackId = trackId
     self.website = website if website else 'UNK'
     self.app_type = app_type 
+    self.category = category
 class AppInfos:
   def __init__(self):
     sqldao = SqlDao()
     QUERY = 'SELECT package_name, app_title, offered_by, category_code, website FROM android_app_details'
     self.apps = {}
     for package_name, app_title, offered_by, category_code, website in sqldao.execute(QUERY):
-      appInfo = App(package_name, app_title, offered_by, 'UNK', website, consts.ANDROID)
+      appInfo = App(package_name, app_title, offered_by, 'UNK', website, category_code,consts.ANDROID)
       self.apps[package_name] = appInfo
     QUERY = 'SELECT trackId, bundleId, trackName,artistName, primaryGenreName, sellerUrl FROM ios_app_details'
     for trackId, bundleId, trackName, artistName, primaryGenreName, sellerUrl in sqldao.execute(QUERY):
         trackId = str(trackId)
-        appInfo = App(bundleId, trackName, artistName, trackId, sellerUrl, consts.IOS)
+        appInfo = App(bundleId, trackName, artistName, trackId, sellerUrl, primaryGenreName,consts.IOS)
         self.apps[trackId] = appInfo
     sqldao.close()
 
