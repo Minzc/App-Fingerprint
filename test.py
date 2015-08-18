a6a7d3a9af0a384ba351f2f1c00f7c963c495d40
Minzc <congzicun@gmail.com>
2015-08-18 13:50:24 -0700

    add app type to test

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from run import cross_batch_test
+import consts
 
 tbls = ['ios_packages_2015_08_12', 'ios_packages_2015_08_10', 'ios_packages_2015_08_04', 'ios_packages_2015_06_08']
 
@@ -13,7 +14,7 @@ for test_tbl in tbls:
 	#if test_tbl != 'packages_20150429':
 	#	continue
 	print train_tbls, test_tbl
-	output = cross_batch_test(train_tbls, test_tbl)
+	output = cross_batch_test(train_tbls, test_tbl, consts.IOS)
 	fw.write(str(train_tbls)+' ' + test_tbl+'\n')
 	fw.write(output + '\n')
 	fw.write('=' * 20 + '\n')
