f7cdfb5c9eecc6ede3838f5ce8172722e139e283
Minzc <congzicun@gmail.com>
2015-09-01 14:12:07 -0700

    add - to agent regex

--- classifiers/agent_classifier.py
+++ classifiers/agent_classifier.py
@@ -9,7 +9,7 @@ test_str = 'NBC'.lower()
 
 class AgentClassifier(AbsClassifer):
     def clean_agent(self, agent):
-      return re.findall('[a-zA-Z][0-9a-zA-Z. %]+', agent)
+      return re.findall('[a-zA-Z][0-9a-zA-Z. %_-]+', agent)
 
     def __init__(self):
       self.agentLabel = defaultdict(set)
--- run.py
+++ run.py
@@ -26,9 +26,9 @@ trainedLabel = {
 trainedClassifiers = [
     #consts.HEAD_CLASSIFIER,
     consts.AGENT_CLASSIFIER,
-    consts.HOST_CLASSIFIER,
-    consts.CMAR_CLASSIFIER,
-    consts.KV_CLASSIFIER,
+    #consts.HOST_CLASSIFIER,
+    #consts.CMAR_CLASSIFIER,
+    #consts.KV_CLASSIFIER,
 ]
 
 def load_trian(size):
