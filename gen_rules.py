a6d398e6ba547ce1647c84b7609729721edf7308
Minzc <congzicun@gmail.com>
2015-09-01 13:11:21 -0700

    add generation methods

--- gen_rules.py
+++ gen_rules.py
@@ -22,8 +22,7 @@ def generate_agent_rules():
 
   for ruleType in classifier.rules:
     for agentFeature, label in classifier.rules[ruleType].items():
-      pcre = 'User-Agent:.*' + agentFeature
-      pcre = re.escape(pcre)
+      pcre = re.escape('User-Agent:')+'.*' + re.escape(agentFeature)
       fileWriter.write(ruleTmplate % (vulnID, 1, label, 1, iosGroup, 'tcp', 'HTTP', 'from_client', pcre, 'header', 10))
       vulnID += 1
   fileWriter.close()
