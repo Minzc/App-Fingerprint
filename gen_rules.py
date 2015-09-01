1f7bd1cf3da220669ad9e77e64fff51866352381
Minzc <congzicun@gmail.com>
2015-09-01 13:03:26 -0700

    add generation methods

--- gen_rules.py
+++ gen_rules.py
@@ -1,6 +1,7 @@
 from classifier_factory import classifier_factory
 import consts
 import re
+ruleTmplate = 'F-SBID( --vuln_id %s; --attack_id %s; --name "%s"; --revision %s; --group %s; --protocol %s; --service %s; --flow %s; --pcre "/%s/i"; --context %s;  --weight %s;)\n'
 def generate_agent_rules():
   fileWriter = open('agent.rule.head', 'w')
   fileWriter.write('F-SGROUP( --name ios_app; )\n')
@@ -12,7 +13,6 @@ def generate_agent_rules():
       #consts.KV_CLASSIFIER,
   ]
 
-  ruleTmplate = 'F-SBID( --vuln_id %s; --attack_id %s; --name "%s"; --revision %s; --group %s; --protocol %s; --service %s; --flow %s; --pcre "/%s/i"; --context header; )\n'
   appType = consts.IOS
   classifier = classifier_factory(trainedClassifiers, appType)[0][1]
   classifier.load_rules()
@@ -23,9 +23,9 @@ def generate_agent_rules():
     for agentFeature, label in classifier.rules[ruleType].items():
       pcre = 'User-Agent:.*' + agentFeature
       pcre = re.escape(pcre)
-      fileWriter.write(ruleTmplate % (vulnID, 1, label, 1, iosGroup, 'tcp', 'HTTP', 'from_server', pcre))
+      fileWriter.write(ruleTmplate % (vulnID, 1, label, 1, iosGroup, 'tcp', 'HTTP', 'from_client', pcre, 'header', 10))
       vulnID += 1
-  fileWriter.close
+  fileWriter.close()
 
 def generate_host_rules():
   fileWriter = open('host.rule.head', 'w')
@@ -38,7 +38,6 @@ def generate_host_rules():
       #consts.KV_CLASSIFIER,
   ]
 
-  ruleTmplate = 'F-SBID( --vuln_id %s; --attack_id %s; --name "%s"; --revision %s; --group %s; --protocol %s; --service %s; --flow %s; --pcre "/%s/i"; --context host; )\n'
   appType = consts.IOS
   classifier = classifier_factory(trainedClassifiers, appType)[0][1]
   classifier.load_rules()
@@ -46,9 +45,36 @@ def generate_host_rules():
   iosGroup = 'ios_app'
 
   for ruleType in classifier.rules:
+    for host, label in classifier.rules[ruleType].items():
+      pcre = host
+      pcre = re.escape(pcre)
+      fileWriter.write(ruleTmplate % (vulnID, 1, label, 1, iosGroup, 'tcp', 'HTTP', 'from_client', pcre, 'host', 9))
+      vulnID += 1
+  fileWriter.close()
+
+def generate_path_rules():
+  fileWriter = open('host.rule.head', 'w')
+  fileWriter.write('F-SGROUP( --name ios_app; )\n')
+  trainedClassifiers = [
+      #consts.HEAD_CLASSIFIER,
+      #consts.AGENT_CLASSIFIER,
+      #consts.HOST_CLASSIFIER,
+      consts.CMAR_CLASSIFIER,
+      #consts.KV_CLASSIFIER,
+  ]
+
+  appType = consts.IOS
+  classifier = classifier_factory(trainedClassifiers, appType)[0][1]
+  classifier.load_rules()
+  vulnID = 300001
+  iosGroup = 'ios_app'
+
+  for ruleType in classifier.rules:
     for agentFeature, label in classifier.rules[ruleType].items():
       pcre = 'User-Agent:.*' + agentFeature
       pcre = re.escape(pcre)
-      fileWriter.write(ruleTmplate % (vulnID, 1, label, 1, iosGroup, 'tcp', 'HTTP', 'from_server', pcre))
+      fileWriter.write(ruleTmplate % (vulnID, 1, label, 1, iosGroup, 'tcp', 'HTTP', 'from_client', pcre, 'uri', 8))
       vulnID += 1
-  fileWriter.close
+  fileWriter.close()
+
+generate_agent_rules()
