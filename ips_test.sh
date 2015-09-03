758dd03c56825904e6f083a1494a2d2961b9509e
Minzc <congzicun@gmail.com>
2015-09-03 15:00:49 -0700

    correct ipsc name

--- ips_test.sh
+++ ips_test.sh
@@ -1,7 +1,7 @@
 RULE_FILE_NAME='kv.rule.head'
 RULE_FILE_NAME_BIN = "$RULE_FILE_NAME".bin
 FILES='~/pcaps/ios/usa/mingxiao1998-outlook/20150812/'
-./ips $RULE_FILE_NAME $RULE_FILE_NAME_BIN
+./ipsc $RULE_FILE_NAME $RULE_FILE_NAME_BIN
 for f in $FILES:
 do 
   echo 'Processing', $f
