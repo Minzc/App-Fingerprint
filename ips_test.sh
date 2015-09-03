d88e9b275de9ccc357af7483991a9cdc57cd9e6e
Minzc <congzicun@gmail.com>
2015-09-03 15:17:39 -0700

    hard code direcoty

--- ips_test.sh
+++ ips_test.sh
@@ -3,7 +3,7 @@ RULE_FILE_NAME='kv.rule.head'
 RULE_FILE_NAME_BIN="$RULE_FILE_NAME".bin
 FILES=~/pcaps/ios/usa/mingxiao1998-outlook/20150812/*
 ./ipsc $RULE_FILE_NAME $RULE_FILE_NAME_BIN
-for f in $FILES:
+for f in ~/pcaps/ios/usa/mingxiao1998-outlook/20150812/*
 do 
   echo 'Processing', $f
 done
