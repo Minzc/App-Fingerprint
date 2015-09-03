0ac3fbf84c05d3288f21a60180ab17592bb1d307
Minzc <congzicun@gmail.com>
2015-09-03 15:02:28 -0700

    correct path

--- ips_test.sh
+++ ips_test.sh
@@ -1,6 +1,6 @@
 RULE_FILE_NAME='kv.rule.head'
 RULE_FILE_NAME_BIN="$RULE_FILE_NAME".bin
-FILES='~/pcaps/ios/usa/mingxiao1998-outlook/20150812/'
+FILES=~/pcaps/ios/usa/mingxiao1998-outlook/20150812/*
 ./ipsc $RULE_FILE_NAME $RULE_FILE_NAME_BIN
 for f in $FILES:
 do 
