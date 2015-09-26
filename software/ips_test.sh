40da45ea05a29d4009fe4233bedae0d3b03397a2
Minzc <congzicun@gmail.com>
2015-09-26 12:29:23 -0700

    change test

--- software/ips_test.sh
+++ software/ips_test.sh
@@ -3,7 +3,7 @@ RULE_FILE_NAME='../ios_rules/ios_usa_agent.rule'
 RULE_FILE_NAME_BIN="$RULE_FILE_NAME".bin
 FILES=~/pcaps/ios/usa/mingxiao1998-outlook/20150812/*
 ./ipsc $RULE_FILE_NAME $RULE_FILE_NAME_BIN
-for f in ~/pcaps/ios/usa/mingxiao1998-outlook/20150812/467154094.pcap
+for f in ~/pcaps/ios/usa/mingxiao1998-outlook/20150812/396659191.pcap
 do 
   echo 'Processing', $f
   ./iscan -akpvr $RULE_FILE_NAME_BIN $f
