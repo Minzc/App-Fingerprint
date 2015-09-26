5480a7068e88ca9198d5e2093c99eb9c12d88f7e
Minzc <congzicun@gmail.com>
2015-09-26 13:59:54 -0700

    add rules

--- software/ips_test.sh
+++ software/ips_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-RULE_FILE_NAME='../ios_rules/ios_usa_agent.rule'
+RULE_FILE_NAME='../ios_rules/ios_usa_host.rule'
 RULE_FILE_NAME_BIN="$RULE_FILE_NAME".bin
 FILES=~/pcaps/ios/usa/mingxiao1998-outlook/20150812/*
 ./ipsc $RULE_FILE_NAME $RULE_FILE_NAME_BIN
-for f in ~/pcaps/ios/usa/mingxiao1998-outlook/20150812/396659191.pcap
+for f in ~/pcaps/ios/usa/mingxiao1998-outlook/20150812/647803670.pcap
 do 
   echo 'Processing', $f
   ./iscan -akpvr $RULE_FILE_NAME_BIN $f
