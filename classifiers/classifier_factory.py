7a26410e728551909d2c24aba48a76a0125510e7
Minzc <congzicun@gmail.com>
2015-10-21 23:56:40 -0700

    change algo_modify to algo

--- classifiers/classifier_factory.py
+++ classifiers/classifier_factory.py
@@ -1,5 +1,5 @@
 from classifiers.agent_classifier_modify import AgentClassifier
-from classifiers.algo_modify import KVClassifier
+from classifiers.algo import KVClassifier
 from classifiers.head import HeaderClassifier
 from classifiers.host import HostApp
 from classifiers.fp_modify import CMAR
