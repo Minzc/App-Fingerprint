f53cc2ab558c55841bea253c48423da290db1856
Minzc <congzicun@gmail.com>
2015-10-31 00:09:44 -0700

    add non occur feature

--- classifiers/agent.py
+++ classifiers/agent.py
@@ -138,12 +138,12 @@ class AgentClassifier(AbsClassifer):
         features = re.findall('^[a-zA-Z0-9][0-9a-zA-Z. %_\-:&?\']+/', agent)
         if len(features) > 0:
           feature = features[0]
-        regexObj = re.compile(r'^' + re.escape(feature), re.IGNORECASE)
-        try:
-          feature = feature.encode('utf-8')
-          appFeatureRegex[app]['#'+ feature] = regexObj
-        except:
-          pass
+          regexObj = re.compile(r'^' + re.escape(feature), re.IGNORECASE)
+          try:
+            feature = feature.encode('utf-8')
+            appFeatureRegex[app]['#'+ feature] = regexObj
+          except:
+            pass
 
     return appFeatureRegex
     
