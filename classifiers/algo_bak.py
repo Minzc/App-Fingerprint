c0cab956fd77c902d100248c64282ef277458dab
Minzc <congzicun@gmail.com>
2015-11-04 02:48:24 -0800

    bug fix

--- classifiers/algo_bak.py
+++ classifiers/algo_bak.py
@@ -277,7 +277,7 @@ class KVClassifier(AbsClassifer):
     # Persist rules
     #############################
     self.persist(specificRules, rule_type)
-    self._compare(matchedHighConfRules, appSpecificRules, hostSec)
+    self._compare(matchedHighConfRules, appSpecificRules, hostSec, appKeyScore)
     self.__init__(self.appType)
     return self
 
