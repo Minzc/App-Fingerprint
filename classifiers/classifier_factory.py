ffb6c4f779b8c46bc6f1703110ad87cdbdc04b4d
Minzc <congzicun@gmail.com>
2015-11-04 15:09:05 -0800

    test refector

--- classifiers/classifier_factory.py
+++ classifiers/classifier_factory.py
@@ -1,4 +1,4 @@
-from classifiers.agent import AgentClassifier
+from classifiers.agent_refactor import AgentClassifier
 from classifiers.algo import KVClassifier
 from classifiers.head import HeaderClassifier
 from classifiers.host import HostApp
@@ -11,13 +11,13 @@ def classifier_factory(names, appType):
     if name == consts.HEAD_CLASSIFIER:
       classifier = HeaderClassifier()
     elif name == consts.AGENT_CLASSIFIER:
-      classifier = AgentClassifier(inferFrmData = False, sampleRate = 2)
+      classifier = AgentClassifier(inferFrmData = False, sampleRate = 0.5)
     elif name == consts.HOST_CLASSIFIER:
       classifier = HostApp(appType)
     elif name == consts.CMAR_CLASSIFIER:
       classifier = CMAR(min_cover = 1)
     elif name == consts.KV_CLASSIFIER:
-      classifier = KVClassifier(appType)
+      classifier = KVClassifier(appType, inferFrmData = False, sampleRate = 0.5)
     classifiers.append((name, classifier))
   return classifiers
 
