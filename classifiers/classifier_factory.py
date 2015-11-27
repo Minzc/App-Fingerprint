b8d65c34da3eb27bd3d0fe4fde6d90a37873608f
Minzc <congzicun@gmail.com>
2015-11-27 10:51:09 -0800

    compare

--- classifiers/classifier_factory.py
+++ classifiers/classifier_factory.py
@@ -1,5 +1,5 @@
 from classifiers.agent import AgentClassifier
-from classifiers.algo_bak import KVClassifier
+from classifiers.algo import KVClassifier
 from classifiers.head import HeaderClassifier
 from classifiers.host import HostApp
 from classifiers.fp_modify import CMAR
