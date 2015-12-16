c72bc8d2ae72d737f28d8a3ed38a636220101f86
Minzc <congzicun@gmail.com>
2015-12-15 21:00:46 -0800

    use agent to train

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ VALID_LABEL = {
 
 USED_CLASSIFIERS = [
     # consts.HEAD_CLASSIFIER,
-    #consts.AGENT_CLASSIFIER,
+    consts.AGENT_CLASSIFIER,
     #consts.KV_CLASSIFIER,
     #consts.URI_CLASSIFIER,
     consts.CMAR_CLASSIFIER,
