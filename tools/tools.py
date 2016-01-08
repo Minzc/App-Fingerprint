7efdd3ef856df0e5991ffa59a6a7a8101fce2479
Minzc <congzicun@gmail.com>
2016-01-08 10:17:03 -0800

    stat undetectable apps

--- tools/tools.py
+++ tools/tools.py
@@ -4,6 +4,36 @@ import sys
 from sqldao import SqlDao
 from collections import defaultdict
 
+def stat_all():
+    target = {'/pep/gcc':'gsp1.apple.com',
+              '/assets/com_apple_MobileAsset_Duet/com_apple_MobileAsset_Duet.xml':'mesu.apple.com',
+              '/configurations/pep/pipeline/pipeline0.html':'configuration.apple.com',
+              '/bag':'init-p01st.push.apple.com'}
+    tbls = ['ios_packages_2015_09_14', 'ios_packages_2015_08_10', 'ios_packages_2015_06_08', 'ios_packages_2015_08_12',
+        'ios_packages_2015_08_04', 'ios_packages_2015_10_16','ios_packages_2015_10_21']
+    sqldao = SqlDao()
+
+    counter = defaultdict(set)
+    for tbl in tbls:
+        SQL = 'select app, path, hst, agent from %s where classified = 3' % tbl
+        https = defaultdict(dict)
+        for app, path, host, agent in sqldao.execute(SQL):
+            https[app][path] = host+'$'+agent
+
+        badapps = set()
+        for app in https:
+            cannotfind = True
+            for path in https[app]:
+                if path not in target:
+                    cannotfind = False
+            if cannotfind == True:
+                badapps.add(app)
+        for app in badapps:
+            counter[app].add(tbl)
+    for app in counter:
+        if len(counter[app]) == len(tbls):
+            print '[BAD APP]', app
+
 def stat_classify_rst(tbl):
     target = {'/pep/gcc':'gsp1.apple.com',
               '/assets/com_apple_MobileAsset_Duet/com_apple_MobileAsset_Duet.xml':'mesu.apple.com',
@@ -38,4 +68,6 @@ if __name__ == '__main__':
         print 'error'
     elif sys.argv[1] == 'stat':
         stat_classify_rst(sys.argv[2])
+    elif sys.argv[1] == 'all':
+        stat_all()
 
